1d9de9f5199c5137805fdc264ea426a5bda84f2c
unknown <ronak71099@gmail.com>
2024-01-29 09:42:31 +0530

    "updating the work of week2 with mongo  and middlewares"

--- week-2/middlewares/errorcount.js
+++ week-2/middlewares/errorcount.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
-const assert = require('assert');
+// const request = require('supertest');
+// const assert = require('assert');
 const express = require('express');
 
 const app = express();
@@ -9,6 +9,13 @@ let errorCount = 0;
 // Your task is to
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+
+app.use((err, req, res, next) => {
+  if(err){
+    errorCount++;
+    res.status(404).send({error:'not found'})
+  }
+})
 
 app.get('/user', function(req, res) {
   throw new Error("User not found");
--- week-2/mongo/routes/user.js
+++ week-2/mongo/routes/user.js
@@ -1,18 +1,60 @@
 const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin");
+const mongoose = require('mongoose');
+const { Admin, User, Course } = require('../db/index');
+const userMiddleware = require("../middleware/user");
+const express = require('express');
+const app = express();
 const router = Router();
-
+app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/shopping');
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
   // Implement admin signup logic
+  try {
+    let username = req.body.username;
+    let password = req.body.password;
+  
+    let signupData = {
+      username,
+      password
+    }
+    let userData = new User(signupData);
+    await userData.save();
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal Server Error");
+  }
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', userMiddleware,async (req, res) => {
   // Implement course creation logic
+  if(req.user){
+    let title = req.body.title;
+    let course = await Course.find({title});
+    if(!course){
+      res.json("invalid course");
+    }
+    else{
+      res.json("course paaa gaya");
+    }
+  }else{
+    res.json("Invalid User");
+  }
+
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-  // Implement fetching all courses logic
+router.get('/courses', userMiddleware, async (req, res) => {
+  try {
+    // Fetch all courses logic here
+    let courses = await Course.find();
+    res.status(200).json(courses);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
